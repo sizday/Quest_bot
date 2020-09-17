a962bfbcb6f48c73b0d7bfe17e8655ac14bc9fa3
sizday <dsizov1999@mail.ru>
2020-09-17 20:34:16 +0300

    Add stickers and delete grammatical mistakes

--- handlers.py
+++ handlers.py
@@ -3,6 +3,8 @@ from aiogram.dispatcher.filters import CommandStart
 from keyboards import transition_menu
 from state import Game
 from load_all import dp
+from sticker_id import positive_sticker, negative_sticker
+import random
 
 number_stage = 1
 admin_id = 382505606
@@ -19,7 +21,7 @@ async def start(message: Message):
                          'Поэтому мы придумали для тебя мини-квест, который тебе предстоит пройти на большом белом '
                          'лимузине! Кстати, он уже ждет тебя. Спускайся вниз!  Для того, чтобы собрать нас всех вместе,'
                          ' тебе нужно будет хорошо подумать и вспомнить самые яркие моменты жизни в городе двух N. '
-                         'Чтобы узнать , в какое место тебе нужно будет ехать, ты должна разгадать загадку. ')
+                         'Чтобы узнать , в какое место тебе нужно будет ехать, ты должна разгадать загадку.')
     await message.answer('Когда будешь готова, нажми кнопку снизу', reply_markup=transition_menu)
     await Game.pre_stage1.set()
 
@@ -34,12 +36,14 @@ async def pre_stage1(message: Message):
 async def stage1(message: Message):
     global number_stage
     if message.text.lower() != 'школа' and message.text.lower() != 'лицей':
+        await message.answer_sticker(negative_sticker.get(random.randint(0, 9)))
         await message.answer('Ответ не верен')
         await Game.stage1.set()
     else:
         number_stage += 1
-        await message.answer('Молодец! Отправляйся по адресу ул. красных зорь 14а, к магазину Пятерочка.'
-                             'Нажми, когда будешь на месте', reply_markup=transition_menu)
+        await message.answer_sticker(positive_sticker.get(number_stage))
+        await message.answer('Молодец! Отправляйся по адресу ул. красных зорь 14а, к магазину Пятерочка.\n'
+                             'Нажми, когда будешь на месте.', reply_markup=transition_menu)
         await Game.pre_stage2.set()
 
 
@@ -53,11 +57,13 @@ async def pre_stage2(message: Message):
 async def stage2(message: Message):
     global number_stage
     if message.text.lower() != "режим" and message.text.lower() != "спортклуб режим":
+        await message.answer_sticker(negative_sticker.get(random.randint(0, 9)))
         await message.answer('Ответ не верен')
         await Game.stage2.set()
     else:
         number_stage += 1
-        await message.answer('Отлично! Адрес места назначения проспект Героев 72А, парковка у Евроспара '
+        await message.answer_sticker(positive_sticker.get(number_stage))
+        await message.answer('Отлично! Адрес места назначения проспект Героев 72А, парковка у Евроспара.\n'
                              'Нажми, когда будешь на месте', reply_markup=transition_menu)
         await Game.pre_stage3.set()
 
@@ -72,12 +78,14 @@ async def pre_stage3(message: Message):
 @dp.message_handler(state=Game.stage3)
 async def stage3(message: Message):
     global number_stage
-    if message.text.lower() != 'сормовский парк' and message.text.lower() != 'стадион труд':
+    if message.text.lower() not in ['сормовский парк', 'стадион труд', 'труд']:
+        await message.answer_sticker(negative_sticker.get(random.randint(0, 9)))
         await message.answer('Ответ не верен')
         await Game.stage3.set()
     else:
         number_stage += 1
-        await message.answer('Молодец! Отправляйся за следующей подружкой на  Бульвар Юбилейный, 30, стадион «Труд.'
+        await message.answer_sticker(positive_sticker.get(number_stage))
+        await message.answer('Молодец! Отправляйся за следующей подружкой на  Бульвар Юбилейный, 30, стадион «Труд.\n'
                              'Нажми, когда будешь на месте', reply_markup=transition_menu)
         await Game.pre_stage4.set()
 
@@ -92,18 +100,20 @@ async def pre_stage4(message: Message):
 async def stage4(message: Message):
     global number_stage
     if message.text.lower() not in ['papi', 'ресторан papi', 'кафе papi']:
+        await message.answer_sticker(negative_sticker.get(random.randint(0, 9)))
         await message.answer('Ответ не верен')
         await Game.stage4.set()
     else:
         number_stage += 1
-        await message.answer('Perfettamente! Езжай по адресу ул. Рождественская, 37'
+        await message.answer_sticker(positive_sticker.get(number_stage))
+        await message.answer('Perfettamente! Езжай по адресу ул. Рождественская, 37\n'
                              'Нажми, когда будешь на месте', reply_markup=transition_menu)
         await Game.pre_stage5.set()
 
 
 @dp.message_handler(state=Game.pre_stage5)
 async def pre_stage5(message: Message):
-    await message.answer('Пятая загадка: место, которое вывело твою жизнь на новый этап')
+    await message.answer('Пятая загадка: место, которое вывело твою жизнь на новый этап.')
     await Game.stage5.set()
 
 
@@ -111,8 +121,10 @@ async def pre_stage5(message: Message):
 async def stage5(message: Message):
     global number_stage
     if message.text.lower() != "пакгаузы":
+        await message.answer_sticker(negative_sticker.get(random.randint(0, 9)))
         await message.answer('Ответ не верен')
         await Game.stage5.set()
     else:
         number_stage += 1
-        await message.answer('Поздравляем! Осталась последняя локация! Езжайте на мыс Стрелка')
+        await message.answer_sticker(positive_sticker.get(number_stage))
+        await message.answer('Поздравляем! Осталась последняя локация! Езжай на мыс Стрелка!')
